7e62e03b6c235cc95497b3a260573db26827904d
giordan-zeina1 <giordan.zeina@stu.ocsb.ca>
2025-02-25 19:39:01 +0000

    code

---
+++ design/main.c
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 Giordan Zeina All rights reserved
+//
+// Created by: Giordan Zeina
+// Created on: Feb 2025
+// This program calculates the circumference of a circle
+// with a diameter of 11 mm.
+#include <stdio.h>
+#include <math.h>
+int main() {
+    // this function calculates the circumference of a 11 mm diameter circle
+    printf("If a circle has a diameter of 11 mm: \n");
+    printf("\n");
+    printf("Circumference is %.5f mm.\n", (M_PI * 11));
+    printf("\nDone.\n");
