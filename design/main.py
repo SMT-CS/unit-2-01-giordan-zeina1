f0b54f519fab93c3a0155f2204e4eb53b98a6762
giordan-zeina1 <giordan.zeina@stu.ocsb.ca>
2025-02-25 19:11:35 +0000

    code

---
+++ design/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Created by: Giordan Zeina
+Created on: Feb 2025
+This module calculates the circumference of a circle
+    with diameter 11 mm.
+"""
+
+import math
+
+
+def main() -> None:
+    """
+    The main() function calculates the circumference of a 11 mm circle,
+        returns None.
+    """
+    print("If a circle has a diameter of 11 mm:")
+    print("")
+    print(f"circumference is {(math.pi * 11):.2f} mm².")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
